8768fd5a4dac47b40d89225e424bce990e5c0c9b
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2020-10-13 17:28:00 -0500

    login and layout

---
+++ src/components/layout-items/FooterView.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+
+const { Footer } = Layout;
+
+class FooterView extends Component {
+    render() {
+        return <Footer style={{ textAlign: 'center' }}>
+            Kimirina ©2020 Created by Los duros del software
+        </Footer>
+    }
+}
+
+export default FooterView;
