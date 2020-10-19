755035e566ce9b341b14f4684a8bbe674a77e6da
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2020-10-19 14:15:05 -0500

    estilos e intento de rutas

--- src/components/layout-items/HeaderView.tsx
+++ src/components/layout-items/HeaderView.tsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import { Layout, PageHeader, Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Layout, PageHeader, Menu, Dropdown, Avatar } from 'antd';
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
 import '../LayoutView.css';
 
 const { Header } = Layout;
+
+const menu = (
+    <Menu>
+      <Menu.Item danger> <p>Cerrar sessión <LogoutOutlined /></p></Menu.Item>
+    </Menu>
+  );
 
 class HeaderView extends Component {
     render() {
         return <Header className="site-layout-sub-header-background" style={{ padding: 0 }}>
                 <PageHeader
-                ghost={false}
                 onBack={() => window.history.back()}
                 title="Title"
                 extra={[
-                    <Avatar key="1" size="large" icon={<UserOutlined />} />,
+                    <Dropdown key="1" overlay={menu}>
+                        <a className="ant-dropdown-link" href="/*" onClick={e => e.preventDefault()}>
+                            <Avatar size="large" icon={<UserOutlined />}  />
+                        </a>
+                    </Dropdown>,
                 ]}
                 >
                 </PageHeader>
