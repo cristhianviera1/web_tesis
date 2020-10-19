abb1222badb3f2018da57153f7f20ddab1de7b19
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2020-10-19 18:37:44 -0500

    routes

--- src/components/LayoutView.tsx
+++ src/components/LayoutView.tsx
@@ -1,51 +1,77 @@
 import React, { Component } from 'react';
 import { Layout, Menu, Image } from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { IdcardOutlined, NotificationOutlined, AppstoreOutlined } from '@ant-design/icons';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// Views
 import  HeaderView  from './layout-items/HeaderView';
-import  ContentView  from './layout-items/ContentView';
 import  FooterView  from './layout-items/FooterView';
+import Users from '../pages/admin/users/Users';
 import './LayoutView.css';
 
-const { Sider } = Layout;
+const { Sider, Content } = Layout;
 
 class LayoutView extends Component {
     render() {
         return <Layout>
-            <Sider 
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={(broken: any) => {
-                    console.log(broken);
-                }}
-                onCollapse={(collapsed: any, type: any) => {
-                    console.log(collapsed, type);
-                }}>
-           
-            <div style={{ height: "10%", margin: "16px" }}>
-                <Image width={"100%"} src={require("../assets/logos/logo-largo.png")}/>
-            </div>
-            <img src="../assets/logos/logo-circular.png" alt=""/> 
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                <Menu.Item key="1" icon={<UserOutlined />}>
-                nav 1
-                </Menu.Item>
-                <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                nav 2
-                </Menu.Item>
-                <Menu.Item key="3" icon={<UploadOutlined />}>
-                nav 3
-                </Menu.Item>
-                <Menu.Item key="4" icon={<UserOutlined />}>
-                nav 4
-                </Menu.Item>
-            </Menu>
-            </Sider>
-            <Layout>
-                <HeaderView/>
-                <ContentView/>
-                <FooterView/>
-            </Layout>
+            <Router>
+                <Sider breakpoint="lg" collapsedWidth="0"
+                    onBreakpoint={(broken: any) => {
+                        //console.log(broken);
+                    }}
+                    onCollapse={(collapsed: any, type: any) => {
+                        //console.log(collapsed, type);
+                    }}>
+            
+                    <div style={{ height: "10%", margin: "16px" }}>
+                        <Image width={"100%"} src={require("../assets/logos/logo-largo.png")}/>
+                    </div>
+                    <Menu theme="dark" mode="inline">
+                        {routes?.map((routeData, index) => {
+                            return (
+                            <Menu.Item key={index} icon={routeData.icon}><Link to={routeData.path}>{routeData.name}</Link></Menu.Item>
+                            )
+                        })}
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <HeaderView/>
+                    <Content style={{ margin: "24px 16px 0" }}>
+                        <div style={{ padding: 24, minHeight: 500 }}>
+                            <Switch>
+                                {routes.map((route, index) => (
+                                <Route key={index} path={route.path} exact={route.exact} children={<route.main />}/>
+                                ))}
+                            </Switch>
+                        </div>
+                    </Content>
+                    <FooterView/>
+                </Layout>
+            </Router>
         </Layout>
     }
 }
 export default LayoutView;
+
+
+const routes = [
+    {
+      path: "/administrator/users",
+      exact: true,
+      name: "Usuarios",
+      icon: <IdcardOutlined />,
+      main: () => <Users/>
+    },
+    {
+      path: "/administrator/news",
+      name: "Noticias",
+      icon: <NotificationOutlined />,
+      main: () => <h2>Bubblegum</h2>
+    },
+    {
+      path: "/administrator/products",
+      name: "Productos",
+      icon: <AppstoreOutlined />,
+      main: () => <h2>Shoelaces</h2>
+    }
+];
